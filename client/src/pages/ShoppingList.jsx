6d62fa3253792e818ef1406671f47a15a1456e32
William <s3864077@rmit.edu.vn>
2022-05-29 21:41:45 +0700

    tuyet voi

--- client/src/pages/ShoppingList.jsx
+++ client/src/pages/ShoppingList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Shopping from "../components/Shopping/Shopping";
 import { useEffect, useState } from "react";
-const HOST = "http://localhost:5000";
+const HOST = import.meta.env.VITE_URL;
 
 let getCategory = (setShopping) => {
   fetch(HOST + "/api/categories")
@@ -25,11 +25,7 @@ function ShoppingList() {
       <div className=" bg-slate-100 flex flex-col flex-1 px-4 py-5 gap-5">
         {shoppings &&
           shoppings.map(({ _id, name, items }) => (
-            <Shopping
-              key={_id}
-              name={name}
-              items={items}
-            ></Shopping>
+            <Shopping key={_id} name={name} items={items}></Shopping>
           ))}
       </div>
     </>
